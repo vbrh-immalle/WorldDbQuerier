de18018f46620de8732230d50746de7ff00d29f0
vbrh_immalle <hans.vanbroeckhoven@immalle.be>
2017-09-28 09:17:08 +0200

    Working argument handling (could be better)

--- WorldDbQuerier/Program.cs
+++ WorldDbQuerier/Program.cs
@@ -4,9 +4,24 @@ namespace WorldDbQuerier
 {
     class Program
     {
+        static string version = "0.1";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(args.Length > 0)
+            {
+                switch(args[0])
+                {
+                    case "-v":
+                        Console.WriteLine("Versie {0}", version);
+                        break;
+                    default:
+                        Console.WriteLine("Onbekend argument");
+                    break;
+                }
+            } else {
+                Console.WriteLine("Hello World");
+            }
         }
     }
 }
